2326337ab68c469d6bd529ca2e8143eafa9405b5
ihyeung <yeung.ireneh@utah.edu>
2019-02-18 18:27:25 -0700

    Created custom exception to be thrown when no match profiles are retrieved in the next batch (may tweak this later, but for now, don't want to proceed with rest of code if an exception is thrown here)

---
+++ src/main/java/com/utahmsd/pupper/exception/EmptyMatchProfileBatchException.java
@@ -0,0 +1,9 @@
+package com.utahmsd.pupper.exception;
+
+public class EmptyMatchProfileBatchException extends RuntimeException {
+    public EmptyMatchProfileBatchException(String message)
+    {
+        super(message);
+    }
+
+}
