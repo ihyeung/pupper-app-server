fdbbe3b3b7be26a202fea4efa2aa7d062d0bb027
ihyeung <yeung.ireneh@utah.edu>
2019-02-23 02:03:13 -0700

    Integration test classes

---
+++ src/test/java/com/utahmsd/pupper/integration/controller/MatchPreferenceControllerIT.java
@@ -0,0 +1,13 @@
+package com.utahmsd.pupper.integration.controller;
+
+import com.utahmsd.pupper.PupperApplication;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ActiveProfiles("TEST")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PupperApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MatchPreferenceControllerIT {
+}
